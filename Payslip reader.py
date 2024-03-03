65e06a39d114139ef032168d92102275d8c51548
Luke Scragg <Luke.scragg@gmail.com>
2024-03-03 00:13:19 +0000

    Created file renamer to maintain naming convention.

--- Payslip reader.py
+++ Payslip reader.py
@@ -1,7 +1,24 @@
-import os, shutil, pdfplumber
+import os, shutil
+from PyPDF2 import PdfReader
 
-Path = r"G:\My Drive\Banking & budgets\Budgets"
+path = r"G:\My Drive\Banking & budgets\Budgets"
 
-for f in os.walk(Path):
-    print(f)
-    
+
+for subdir, dirs, files in os.walk(path):
+    for fname in files:
+        name, ext = os.path.splitext(fname)
+        if ext == "":
+            file_and_path = os.path.join(subdir, fname)
+            path_elements = file_and_path.split("\\")
+            if "2020" not in path_elements[-1] and "2019" not in path_elements[-1]: 
+                print(file_and_path)
+                os.rename(file_and_path, os.path.join(name +" " + path_elements[-3] + ".pdf"))
+
+
+#reader = PdfReader(r'G:\My Drive\Banking & budgets\Budgets\2022\Payslips\1 - January payslip.pdf')
+
+#f = open("demofile2.txt", "a")
+#page = reader.pages[0]
+#print(page.extract_text())
+#f.write(page.extract_text())
+#f.close()
